35c867381c1e1dd87116c19575e9f0739642663d
Oleksii Stetsyk <oleksiy130@gmail.com>
2020-02-21 14:35:19 +0200

    test fixed

--- Treap/main.cpp
+++ Treap/main.cpp
@@ -105,13 +105,13 @@ private:
 void TestTreap(){
     Treap treap;
     vector<int> testVector;
-    for(int i = 0; i < 2; ++i){
+    for(int i = 0; i < 10; ++i){
         treap.insert(i * i);
         testVector.push_back(i * i);
     }
     testVector.push_back(INT_MAX);
 
-    for(int i = 0; i < 20; i += 10){
+    for(int i = 0; i < 100; i += 10){
         ASSERT_EQUAL(treap.lowerBound(i), *lower_bound(testVector.begin(), testVector.end(), i));
     }
 }
