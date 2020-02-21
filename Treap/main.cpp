439c5c4478d21915c1f8e35f0bc854725cd1d65e
Oleksii Stetsyk <oleksiy130@gmail.com>
2020-02-21 15:26:10 +0200

    Tests added

--- Treap/main.cpp
+++ Treap/main.cpp
@@ -8,11 +8,13 @@ using namespace std;
 struct TreapNode{
     int key; // Keys are ordered as in the heap
     int value; // Values are ordered as in the binary search tree
+    int cnt;
     TreapNode * left;
     TreapNode * right;
     TreapNode(int value, int key = rand()){
         this -> value = value;
         this -> key = key;
+        this -> cnt = 1;
         left = right = nullptr;
     }
     ~TreapNode(){
@@ -32,6 +34,15 @@ public:
     int lowerBound(int value){
         return lowerBound(this -> head, value);
     }
+    int findNth(int number){
+        return findNth(this -> head, number);
+    }
+    int pr(){
+        cout << this -> head -> cnt << endl;
+    }
+    void print(){
+        print(head);
+    }
     ~Treap(){
        delete this -> head;
     }
@@ -49,6 +60,8 @@ private:
             r = head;
             Split(r -> left, l, head -> left, value);
         }
+        updCnt(l);
+        updCnt(r);
     }
     void Merge(TreapNode * & head, TreapNode * l, TreapNode * r){
         if(!l || !r){
@@ -67,6 +80,7 @@ private:
             head = l;
             Merge(head -> right, l -> right, r);
         }
+        updCnt(head);
     }
     void Insert(TreapNode * & head, int value){
         TreapNode * l = nullptr;
@@ -100,9 +114,51 @@ private:
         }
         return result;
     }
+    int findNth(TreapNode * head, int number){
+        if(!head || head -> cnt < number){
+            return INT_MIN;
+        }
+        assert(cnt(head) == cnt(head -> left) + cnt(head -> right) + 1);
+        int leftCnt = cnt(head -> left);
+        if (leftCnt + 1 == number){
+            return head -> value;
+        }
+        else if (leftCnt + 1 > number){
+            return findNth(head -> left, number);
+        }
+        else{
+            return findNth(head -> right, number - leftCnt - 1);
+        }
+    }
+    void updCnt(TreapNode * head){
+        if(head) {
+            head->cnt = cnt(head -> left) + cnt(head -> right) + 1;
+        }
+    }
+    int cnt(TreapNode * head){
+        return head ? head -> cnt : 0;
+    }
+    void print(TreapNode * head){
+        if(!head){
+            return;
+        }
+        print(head -> left);
+        print(head -> right);
+        cout << head -> key << " " << head -> value << " " << head -> cnt << endl;
+    }
 };
 
-void TestTreap(){
+void TestLowerBound();
+void TestFindNth();
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, TestLowerBound);
+    RUN_TEST(tr, TestFindNth);
+    return 0;
+}
+
+void TestLowerBound(){
     Treap treap;
     vector<int> testVector;
     for(int i = 0; i < 10; ++i){
@@ -116,8 +172,16 @@ void TestTreap(){
     }
 }
 
-int main() {
-    TestRunner tr;
-    RUN_TEST(tr, TestTreap);
-    return 0;
+void TestFindNth(){
+    Treap treap;
+    vector<int> testVector;
+    for(int i = 0; i < 10; ++i){
+        treap.insert(i * i);
+        testVector.push_back(i * i);
+    }
+    testVector.push_back(INT_MAX);
+
+    for(int i = 1; i <= 10; ++i){
+        ASSERT_EQUAL(treap.findNth(i), testVector[i - 1]);
+    }
 }
